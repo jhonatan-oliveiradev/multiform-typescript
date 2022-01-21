79902c8fd4639d23bc129d69905d7b7c86727f26
JhoNBaratheoN <jhonatan.oliveira931002@gmail.com>
2022-01-21 15:19:59 -0300

    commit inicial

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as C from "./styles";
+
+export const Header = () => {
+  return (
+    <C.Container>
+      <h1>Cadastro de Desenvolvedor</h1>
+      <p>Cadastre seu currículo e as empresas entrarão em contato com você.</p>
+    </C.Container>
+  );
+};
